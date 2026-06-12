eaa0ebc1c4101d496afc1af31b4e8e228f328db1
PearOtters <pierrealexis.outters@gmail.com>
2026-06-12 21:31:07 +0100

    Added a dataset manager file to manage input from binary files

---
+++ ModelTraining/dataset_manager.py
@@ -0,0 +1,26 @@
+import numpy as np
+from torch.utils.data import IterableDataset
+import os
+import glob
+
+class ChunkedChessDataset(IterableDataset):
+    def __init__(self, folder_path):
+        super().__init__()
+        search_path = os.path.join(folder_path, "*.npz")
+        self.chunk_files = sorted(glob.glob(search_path))
+
+        if len(self.chunk_files) == 0:
+            raise ValueError(f"No chunk files found in {folder_path}")
+
+    def __iter__(self):
+        for file_path in self.chunk_files:
+            data = np.load(file_path)
+            packed_boards = data['boards']
+
+            raw_scores = data['scores']
+            scaled_scores = np.tanh(raw_scores / 400.0).astype(np.float32)
+
+            unpacked_boards = np.unpackbits(packed_boards).reshape(-1, 13, 64).astype(np.float32)
+
+            for i in range(len(scaled_scores)):
+                yield unpacked_boards[i], scaled_scores[i]
