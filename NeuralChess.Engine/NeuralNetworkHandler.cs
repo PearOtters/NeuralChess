7c84d4b5190d7091b2c6831405f1f03746bc34db
PearOtters <pierrealexis.outters@gmail.com>
2026-06-13 19:31:52 +0100

    Changed neural network handler to be thread safe for later optimisations

--- NeuralChess.Engine/NeuralNetworkHandler.cs
+++ NeuralChess.Engine/NeuralNetworkHandler.cs
@@ -9,27 +9,31 @@ namespace NeuralChess.Engine
     public class NeuralNetworkHandler
     {
         private static readonly InferenceSession session = new("ChessValueNet.onnx");
-        private static readonly float[] boardState = new float[832];
-        private static readonly DenseTensor<float> inputTensor;
-        private static readonly List<NamedOnnxValue> inputs;
 
-        static NeuralNetworkHandler()
-        {
-            inputTensor = new DenseTensor<float>(boardState, [1, 13, 64]);
-            inputs = [NamedOnnxValue.CreateFromTensor("board_input", inputTensor)];
-        }
+        private static readonly ThreadLocal<float[]> threadLocalBoardState =
+            new(() => new float[832]);
+
+        private static readonly ThreadLocal<DenseTensor<float>> threadLocalInputTensor =
+            new(() => new DenseTensor<float>(threadLocalBoardState.Value, [1, 13, 64]));
+
+        private static readonly ThreadLocal<List<NamedOnnxValue>> threadLocalInputs =
+            new(() => [NamedOnnxValue.CreateFromTensor("board_input", threadLocalInputTensor.Value)]);
 
         public static int GetBoardValue(Board board)
         {
-            board.CalculateBoardState(boardState);
+            float[]? currentBuffer = threadLocalBoardState.Value;
+            if (currentBuffer == null) return 0;
+            board.CalculateBoardState(currentBuffer);
+
             float output = GetNeuralNetworkOutput();
+
             output = Math.Clamp(output, -0.99999f, 0.99999f);
             return (int)Math.Round(Math.Atanh(output) * 400);
         }
 
         public static float GetNeuralNetworkOutput()
         {
-            using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
+            using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(threadLocalInputs.Value);
             return results[0].AsTensor<float>().First();
         }
     }
