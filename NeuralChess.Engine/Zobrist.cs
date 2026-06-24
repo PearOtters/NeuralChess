7fa03509327d9dd84f539c2b6a2eacd72306c91d
PearOtters <pierrealexis.outters@gmail.com>
2026-06-24 21:41:27 +0100

    Create a Zobrist class for a transposition table

---
+++ NeuralChess.Engine/Zobrist.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NeuralChess.Engine
+{
+    public static class Zobrist
+    {
+        public static readonly ulong[,] PieceKeys = new ulong[12, 64];
+        public static readonly ulong SideToMove;
+        public static readonly ulong[] CastlingRights = new ulong[16];
+        public static readonly ulong[] EnPassantSquares = new ulong[8];
+
+        public const int Seed = 1000;
+
+        public static void Initialise()
+        {
+            RandomUlong(new Random(Seed));
+        }
+
+        static Zobrist()
+        {
+            Random rand = new Random(Seed);
+
+            for (int piece = 0; piece < 12; piece++)
+            {
+                for (int square = 0; square < 64; square++)
+                {
+                    PieceKeys[piece, square] = RandomUlong(rand);
+                }
+            }
+
+            SideToMove = RandomUlong(rand);
+
+            for (int i = 0; i < 16; i++) CastlingRights[i] = RandomUlong(rand);
+            for (int i = 0; i < 8; i++) EnPassantSquares[i] = RandomUlong(rand);
+        }
+
+        static ulong RandomUlong(Random rand)
+        {
+            byte[] bytes = new byte[8];
+            rand.NextBytes(bytes);
+            return BitConverter.ToUInt64(bytes);
+        }
+    }
+}
