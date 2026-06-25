68a1efd23d1dd84984b13855d1a178488a5d4c29
PearOtters <pierrealexis.outters@gmail.com>
2026-06-25 11:03:45 +0100

    Added a TTEntry to be added to the hash transposition table

---
+++ NeuralChess.Engine/TTEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralChess.Engine
+{
+    public readonly struct TTEntry
+    {
+        readonly ulong ZobristKey;
+        readonly int Move;
+        readonly short Score;
+        readonly byte Depth;
+        readonly byte NodeFlag;
+    }
+}
